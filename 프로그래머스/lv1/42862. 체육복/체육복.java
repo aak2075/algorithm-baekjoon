54d65e8f6c18de894f392d0a94e057f1ea19d1aa
Gyuseong Lee <lks971003@gmail.com>
2023-09-14 00:26:40 +0900

    [level 1] Title: 체육복, Time: 0.75 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -2,10 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        List<Integer> clothes = new ArrayList<>(Collections.nCopies(n + 1, 1));
-        
-        Arrays.sort(reserve);
-        
+        List<Integer> clothes = new ArrayList<>(Collections.nCopies(31, 1));
         for (Integer i : reserve) {
             clothes.set(i, 2);
         }
@@ -13,30 +10,27 @@ class Solution {
             clothes.set(i, clothes.get(i) - 1);
         }
         
-        for (int i = 0; i < reserve.length; i++) {
-            if (clothes.get(reserve[i]) != 2) {
-                continue;
-            }
-            
-            if (reserve[i] > 1 && clothes.get(reserve[i] - 1) == 0) {
-                clothes.set(reserve[i] - 1, 1);
-                clothes.set(reserve[i], 1);
-                continue;
-            }
-            if (reserve[i] < n && clothes.get(reserve[i] + 1) == 0) {
-                clothes.set(reserve[i] + 1, 1);
-                clothes.set(reserve[i], 1);
+        for (int i = 1; i < clothes.size(); i++) {
+            if (clothes.get(i) > 1) {
+                if (clothes.get(i - 1) == 0) {
+                    clothes.set(i - 1, 1);
+                    clothes.set(i, 1);
+                } else if (clothes.get(i + 1) == 0) {
+                    clothes.set(i + 1, 1);
+                    clothes.set(i, 1);
+                }
             }
         }
         
         int answer = 0;
-        for (int i = 1; i < n + 1; i++) {
-            if (clothes.get(i) > 0) {
+        for (int i = 1; i < 31; i++) {
+            if (clothes.get(i) == 1) {
                 answer++;
             }
         }
         return answer;
     }
+    
 }
 
 // 여벌이 있으면 앞이나 뒤로 빌려줄 수 있다.
